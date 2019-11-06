6edbd39d428140c3bd6bf370a4fb0fd1614332d3
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2019-11-06 15:14:08 -0600

    added signin page

---
+++ frontend/src/changes.bash
@@ -0,0 +1,13 @@
+git rm App.test.tsx
+git rm App.tsx
+git rm index.tsx
+git rm react-app-env.d.ts
+git rm serviceWorker.ts
+git rm ../tsconfig.json
+git add ../package-lock.json
+git add App.js
+git add SignIn.js
+git add changes.bash
+git add index.js
+git add serviceWorker.js
+
